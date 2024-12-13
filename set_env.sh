7d690c7d46abe4511581aeb4154cdfc0d322df46
thanhdu <thanhdu-hcmus@gmail.com>
2024-12-13 12:59:57 +0700

    Initialize a new project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mymasteruser
+export POSTGRES_PASSWORD=mymasterpassword
+export POSTGRES_HOST=my-postgresql-db.cqg1cl7skyby.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=my-dev-bucket-123123
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'Aa123456',
+  'database': 'postgres',
+  'host': 'udacity.cfsoqes9ekvj.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'mynd9990',
+  'url': 'http://localhost:8080',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
